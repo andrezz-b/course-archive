5fdc278da9236f78c270c1e4e1064f89a3bb883f
Andrej Božić <abozic389@gmail.com>
2024-03-01 19:30:26 +0100

    Adding simple security config and users model.

---
+++ backend/src/main/java/com/andrezzb/coursearchive/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.andrezzb.coursearchive.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf((csrf) -> csrf.disable())
+        .authorizeHttpRequests((auth) -> auth
+            .requestMatchers("/api/**").permitAll()
+            .anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults());
+
+    return http.build();
+  }
+
+  @Bean
+  UserDetailsService users() {
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username("admin")
+        .password("password")
+        .roles("ADMIN")
+        .build();
+
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user")
+        .password("password")
+        .roles("USER")
+        .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+
+}
---
+++ backend/src/main/java/com/andrezzb/coursearchive/security/models/Role.java
@@ -0,0 +1,21 @@
+package com.andrezzb.coursearchive.security.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+}
