15a5abb232f4dde4651a622c1d845852df0aae49
Andrej Božić <abozic389@gmail.com>
2024-03-20 08:33:46 +0100

    Adding College JPA entity.

---
+++ backend/src/main/java/com/andrezzb/coursearchive/college/models/College.java
@@ -0,0 +1,46 @@
+package com.andrezzb.coursearchive.college.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "college")
+public class College {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 128)
+    private String name;
+
+    @Column(length = 32)
+    private String acronym;
+
+    @Column(nullable = false, length = 64)
+    private String city;
+
+    @Column(nullable = false)
+    private Short postcode;
+
+    @Column(nullable = false, length = 128)
+    private String address;
+
+    @Column(length = 512)
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
