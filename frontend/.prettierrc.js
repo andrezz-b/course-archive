fff6baf0de0b176ca50a7201b2018d40bf17200d
Andrej Božić <abozic389@gmail.com>
2024-05-19 00:48:28 +0200

    Adding react project.

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  endOfLine: "lf",
+  arrowParens: "always",
+};
+export default config;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-red-400">React</h1>;
+}
+
+export default App;
