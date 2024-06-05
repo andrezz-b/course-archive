db9198b9c1292b452fe8fd3fda02470f1cc7baef
Andrej Božić <abozic389@gmail.com>
2024-06-05 20:20:29 +0200

    Fixing listing page shrinking when there are no results.

--- frontend/src/pages/course/CourseListingPage.tsx
+++ frontend/src/pages/course/CourseListingPage.tsx
@@ -9,78 +9,78 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { useSearchParams } from "react-router-dom";
 
 const CourseListingPage = () => {
-	const [searchParams, setSearchParams] = useSearchParams({
-		filterField: CourseFilter[0].field,
-		filterValue: "",
-		sortField: CourseSort[0].field,
-		sortDirection: CourseSort[0].direction,
-		page: "0",
-	});
-	const query = CourseService.useGetAll(Object.fromEntries(searchParams.entries()), {
-		placeholderData: keepPreviousData,
-	});
+  const [searchParams, setSearchParams] = useSearchParams({
+    filterField: CourseFilter[0].field,
+    filterValue: "",
+    sortField: CourseSort[0].field,
+    sortDirection: CourseSort[0].direction,
+    page: "0",
+  });
+  const query = CourseService.useGetAll(Object.fromEntries(searchParams.entries()), {
+    placeholderData: keepPreviousData,
+  });
 
-	const form = useForm<SearchData>({
-		defaultValues: {
-			filterField: searchParams.get("filterField")!,
-			filterValue: searchParams.get("filterValue")!,
-			sortField: `${searchParams.get("sortField")}-${searchParams.get("sortDirection")}`,
-			idField: searchParams.get("programId") ?? undefined,
-		},
-	});
+  const form = useForm<SearchData>({
+    defaultValues: {
+      filterField: searchParams.get("filterField")!,
+      filterValue: searchParams.get("filterValue")!,
+      sortField: `${searchParams.get("sortField")}-${searchParams.get("sortDirection")}`,
+      idField: searchParams.get("programId") ?? undefined,
+    },
+  });
 
-	const onSubmit: SubmitHandler<SearchData> = useCallback(
-		(data) => {
-			setSearchParams({
-				filterField: data.filterField,
-				filterValue: data.filterValue,
-				sortField: data.sortField.split("-")[0],
-				sortDirection: data.sortField.split("-")[1],
-				programId: data?.idField ?? "",
-				page: "0",
-			});
-		},
-		[setSearchParams],
-	);
+  const onSubmit: SubmitHandler<SearchData> = useCallback(
+    (data) => {
+      setSearchParams({
+        filterField: data.filterField,
+        filterValue: data.filterValue,
+        sortField: data.sortField.split("-")[0],
+        sortDirection: data.sortField.split("-")[1],
+        programId: data?.idField ?? "",
+        page: "0",
+      });
+    },
+    [setSearchParams],
+  );
 
-	const setPage = useCallback(
-		(page: number) => {
-			setSearchParams((prev) => {
-				prev.set("page", page.toString());
-				return prev;
-			});
-		},
-		[setSearchParams],
-	);
+  const setPage = useCallback(
+    (page: number) => {
+      setSearchParams((prev) => {
+        prev.set("page", page.toString());
+        return prev;
+      });
+    },
+    [setSearchParams],
+  );
 
-	useEffect(() => {
-		const subscription = form.watch((_value, { name }) => {
-			if (name === "sortField") {
-				form.handleSubmit(onSubmit)();
-			}
-		});
-		return () => subscription.unsubscribe();
-	}, [form, form.handleSubmit, form.watch, onSubmit]);
-	return (
-		<div className="flex flex-col justify-center items-center">
-			<div className="max-w-[1200px] space-y-4 px-4 md:p-0">
-				<h2 className="text-2xl border-none">Browse Courses</h2>
-				<ListingSearchForm
-					form={form}
-					onSubmit={onSubmit}
-					sortOptions={CourseSort}
-					filterOptions={CourseFilter}
-					idFieldName="Program Id"
-				/>
-				<InfiniteCardList
-					Card={CourseCard}
-					query={query}
-					notFoundMessage="No courses found"
-					setPage={setPage}
-				/>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    const subscription = form.watch((_value, { name }) => {
+      if (name === "sortField") {
+        form.handleSubmit(onSubmit)();
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [form, form.handleSubmit, form.watch, onSubmit]);
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="lg:w-[1000px] space-y-4 px-4 md:p-0">
+        <h2 className="text-2xl border-none">Browse Courses</h2>
+        <ListingSearchForm
+          form={form}
+          onSubmit={onSubmit}
+          sortOptions={CourseSort}
+          filterOptions={CourseFilter}
+          idFieldName="Program Id"
+        />
+        <InfiniteCardList
+          Card={CourseCard}
+          query={query}
+          notFoundMessage="No courses found"
+          setPage={setPage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CourseListingPage;
